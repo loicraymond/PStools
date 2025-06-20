a0e3c45cb24bf89ceb81e1b67d608e8137e93be7
Loïc Raymond <69368464+loicraymond@users.noreply.github.com>
2025-06-20 21:23:44 +0200

    Update Get-Mailbox-with-forward.ps1

--- Office365/Get-Mailbox-with-forward.ps1
+++ Office365/Get-Mailbox-with-forward.ps1
@@ -1,1 +1,5 @@
+# Connexion à Exchange Online
+Connect-ExchangeOnline
+
+# Liste des boites aux lettres avec un transfert
 Get-Mailbox | ? {$_.ForwardingSmtpAddress -ne $null} | FT Name, ForwardingSmtpAddress,DeliverToMailboxandForward
